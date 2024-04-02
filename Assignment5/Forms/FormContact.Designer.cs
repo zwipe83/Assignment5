f67236016896bc0be9e4a7abc722dc101f83d545
Samuel Jeffman <sjeffman83@outlook.com>
2024-04-02 19:44:32 +0200

    Added first draft of FormContact

---
+++ Assignment5/Forms/FormContact.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Assignment5.Forms
+{
+    partial class FormContact
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            grpName = new GroupBox();
+            grpEmailPhone = new GroupBox();
+            grpAddress = new GroupBox();
+            SuspendLayout();
+            // 
+            // grpName
+            // 
+            grpName.Location = new Point(12, 22);
+            grpName.Name = "grpName";
+            grpName.Size = new Size(578, 124);
+            grpName.TabIndex = 0;
+            grpName.TabStop = false;
+            grpName.Text = "Name";
+            // 
+            // grpEmailPhone
+            // 
+            grpEmailPhone.Location = new Point(12, 161);
+            grpEmailPhone.Name = "grpEmailPhone";
+            grpEmailPhone.Size = new Size(578, 209);
+            grpEmailPhone.TabIndex = 0;
+            grpEmailPhone.TabStop = false;
+            grpEmailPhone.Text = "Email and phone";
+            // 
+            // grpAddress
+            // 
+            grpAddress.Location = new Point(12, 376);
+            grpAddress.Name = "grpAddress";
+            grpAddress.Size = new Size(578, 209);
+            grpAddress.TabIndex = 0;
+            grpAddress.TabStop = false;
+            grpAddress.Text = "Address";
+            // 
+            // FormContact
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(602, 722);
+            Controls.Add(grpAddress);
+            Controls.Add(grpEmailPhone);
+            Controls.Add(grpName);
+            Name = "FormContact";
+            Text = "FormContact";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox grpName;
+        private GroupBox grpEmailPhone;
+        private GroupBox grpAddress;
+    }
+}
---
+++ Assignment5/Forms/FormContact.cs
@@ -0,0 +1,27 @@
+﻿/// <summary>
+/// Filename: Program.cs
+/// Created on: 2024-04-02 00:00:00
+/// Author: Samuel Jeffman
+/// </summary>
+/// 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment5.Forms
+{
+    public partial class FormContact : Form
+    {
+        public FormContact()
+        {
+            InitializeComponent();
+        }
+    }
+}
