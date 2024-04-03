25435c92cd5f284a89dd50a4c3671efcf759323c
Samuel Jeffman <sjeffman83@outlook.com>
2024-04-03 21:03:48 +0200

    Added Contact class

---
+++ Assignment5/Classes/Contact.cs
@@ -0,0 +1,42 @@
+﻿namespace Assignment5.Classes
+{
+    public class Email
+    {
+        public Email() { }
+    }
+    public class Phone
+    {
+        public Phone() { }
+    }
+    public class Address
+    {
+        public Address() { }
+    }
+
+    public class Name
+    {
+        public Name() { }
+    }
+
+    public class Contact
+    {
+        private Email businessEmail;
+        private Email privateEmail;
+        private Phone homePhone;
+        private Phone mobilePhone;
+        private Address address;
+        private Name firstName;
+        private Name lastName;
+
+        public Contact()
+        {
+            businessEmail = new Email();
+            privateEmail = new Email();
+            homePhone = new Phone();
+            mobilePhone = new Phone();
+            address = new Address();
+            firstName = new Name();
+            lastName = new Name();
+        }
+    }
+}
