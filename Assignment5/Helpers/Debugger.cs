fa0492de67cd3bcad9b30ed752226e3aecdc9c62
Samuel Jeffman <sjeffman83@outlook.com>
2024-04-04 22:25:55 +0200

    Added helpers

---
+++ Assignment5/Helpers/Debugger.cs
@@ -0,0 +1,24 @@
+﻿/// <summary>
+/// Filename: Debugger.cs
+/// Created on: 2024-04-04 00:00:00
+/// Author: Samuel Jeffman
+/// </summary>
+/// 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5.Helpers
+{
+    internal class Debugger
+    {
+        public static void DEBUG_PRINT(string message)
+        { 
+            DateTime now = DateTime.Now;
+            System.Diagnostics.Debug.Print($"{now:yyyy-MM-dd HH:mm:ss.fff} {message}");
+        }
+    }
+}
---
+++ Assignment5/Helpers/EmailChecker.cs
@@ -0,0 +1,30 @@
+﻿/// <summary>
+/// Filename: Program.cs
+/// Created on: 2024-04-04 00:00:00
+/// Author: Samuel Jeffman
+/// </summary>
+/// 
+using System.Text.RegularExpressions;
+using System.Windows.Navigation;
+
+namespace Assignment5.Helpers
+{
+    public class EmailChecker
+    {
+        public static bool IsValidAddress(string email)
+        {
+            if (email is not null)
+            {
+                // Regular expression pattern for email validation
+                string pattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
+
+                Regex regex = new Regex(pattern);
+
+                Match match = regex.Match(email);
+
+                return match.Success;
+            }
+            return false;
+        }
+    }
+}
