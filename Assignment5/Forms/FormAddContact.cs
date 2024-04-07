ccf322849fb6e278bf3420bc0f088df0bfd49c55
Samuel Jeffman <sjeffman83@outlook.com>
2024-04-07 21:54:10 +0200

    Minor changes

--- Assignment5/Forms/FormAddContact.cs
+++ Assignment5/Forms/FormAddContact.cs
@@ -5,7 +5,7 @@ namespace Assignment5.Forms
     //TODO: Maybe not needed.
     public partial class FormAddContact : FormContact
     {
-        public FormAddContact(Contact contact) : base(contact)
+        public FormAddContact(Contact contact, CustomerManager customerManager) : base(contact, customerManager)
         {
         }
     }
