e348600fd80659831722f418d75fc85ec1029a43
Samuel Jeffman <sjeffman83@outlook.com>
2024-04-07 21:10:15 +0200

    Added classes

---
+++ Assignment5/Classes/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5.Classes
+{
+    internal class Customer
+    {
+        #region Fields
+        private Contact contact;
+        #endregion
+        #region Properties
+        #endregion
+        #region Constructors
+        public Customer()
+        {
+
+        }
+        #endregion
+        #region Methods
+        #endregion
+    }
+}
