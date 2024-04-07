bcabd835f450a33241b98e306ab4b2afd32042e5
Samuel Jeffman <sjeffman83@outlook.com>
2024-04-07 12:20:45 +0200

    Added descriptive names

--- Assignment5/Enums/Countries.cs
+++ Assignment5/Enums/Countries.cs
@@ -7,202 +7,205 @@
 
 namespace Assignment5.Enums
 {
+    /// <summary>
+    /// Represents a list of countries.
+    /// </summary>
     public enum Country
     {
-        Afghanistan,
-        Albania,
-        Algeria,
-        Andorra,
-        Angola,
-        AntiguaAndBarbuda,
-        Argentina,
-        Armenia,
-        Australia,
-        Austria,
-        Azerbaijan,
-        Bahamas,
-        Bahrain,
-        Bangladesh,
-        Barbados,
-        Belarus,
-        Belgium,
-        Belize,
-        Benin,
-        Bhutan,
-        Bolivia,
-        BosniaAndHerzegovina,
-        Botswana,
-        Brazil,
-        BruneiDarussalam,
-        Bulgaria,
-        BurkinaFaso,
-        Burundi,
-        CaboVerde,
-        Cambodia,
-        Cameroon,
-        Canada,
-        CentralAfricanRepublic,
-        Chad,
-        Chile,
-        China,
-        Colombia,
-        Comoros,
-        Congo,
-        CostaRica,
-        CoteDIvoire,
-        Croatia,
-        Cuba,
-        Cyprus,
-        CzechRepublic,
-        DemocraticPeoplesRepublicOfKorea,
-        DemocraticRepublicOfTheCongo,
-        Denmark,
-        Djibouti,
-        Dominica,
-        DominicanRepublic,
-        Ecuador,
-        Egypt,
-        ElSalvador,
-        EquatorialGuinea,
-        Eritrea,
-        Estonia,
-        Eswatini,
-        Ethiopia,
-        Fiji,
-        Finland,
-        France,
-        Gabon,
-        Gambia,
-        Georgia,
-        Germany,
-        Ghana,
-        Greece,
-        Grenada,
-        Guatemala,
-        Guinea,
-        GuineaBissau,
-        Guyana,
-        Haiti,
-        HolySee,
-        Honduras,
-        Hungary,
-        Iceland,
-        India,
-        Indonesia,
-        Iran,
-        Iraq,
-        Ireland,
-        Israel,
-        Italy,
-        Jamaica,
-        Japan,
-        Jordan,
-        Kazakhstan,
-        Kenya,
-        Kiribati,
-        Kuwait,
-        Kyrgyzstan,
-        LaoPeoplesDemocraticRepublic,
-        Latvia,
-        Lebanon,
-        Lesotho,
-        Liberia,
-        Libya,
-        Liechtenstein,
-        Lithuania,
-        Luxembourg,
-        Madagascar,
-        Malawi,
-        Malaysia,
-        Maldives,
-        Mali,
-        Malta,
-        MarshallIslands,
-        Mauritania,
-        Mauritius,
-        Mexico,
-        Micronesia,
-        Monaco,
-        Mongolia,
-        Montenegro,
-        Morocco,
-        Mozambique,
-        Myanmar,
-        Namibia,
-        Nauru,
-        Nepal,
-        Netherlands,
-        NewZealand,
-        Nicaragua,
-        Niger,
-        Nigeria,
-        NorthMacedonia,
-        Norway,
-        Oman,
-        Pakistan,
-        Palau,
-        Panama,
-        PapuaNewGuinea,
-        Paraguay,
-        Peru,
-        Philippines,
-        Poland,
-        Portugal,
-        Qatar,
-        RepublicOfKorea,
-        RepublicOfMoldova,
-        Romania,
-        RussianFederation,
-        Rwanda,
-        SaintKittsAndNevis,
-        SaintLucia,
-        SaintVincentAndTheGrenadines,
-        Samoa,
-        SanMarino,
-        SaoTomeAndPrincipe,
-        SaudiArabia,
-        Senegal,
-        Serbia,
-        Seychelles,
-        SierraLeone,
-        Singapore,
-        Slovakia,
-        Slovenia,
-        SolomonIslands,
-        Somalia,
-        SouthAfrica,
-        SouthSudan,
-        Spain,
-        SriLanka,
-        StateOfPalestine,
-        Sudan,
-        Suriname,
-        Sweden,
-        Switzerland,
-        SyrianArabRepublic,
-        Tajikistan,
-        Thailand,
-        TimorLeste,
-        Togo,
-        Tonga,
-        TrinidadAndTobago,
-        Tunisia,
-        Turkey,
-        Turkmenistan,
-        Tuvalu,
-        Uganda,
-        Ukraine,
-        UnitedArabEmirates,
-        UnitedKingdomOfGreatBritainAndNorthernIreland,
-        UnitedRepublicOfTanzania,
-        UnitedStatesOfAmerica,
-        Uruguay,
-        Uzbekistan,
-        Vanuatu,
-        Venezuela,
-        Vietnam,
-        Yemen,
-        Zambia,
-        Zimbabwe
+        Afghanistan, // Represents the country Afghanistan.
+        Albania, // Represents the country Albania.
+        Algeria, // Represents the country Algeria.
+        Andorra, // Represents the country Andorra.
+        Angola, // Represents the country Angola.
+        AntiguaAndBarbuda, // Represents the country Antigua and Barbuda.
+        Argentina, // Represents the country Argentina.
+        Armenia, // Represents the country Armenia.
+        Australia, // Represents the country Australia.
+        Austria, // Represents the country Austria.
+        Azerbaijan, // Represents the country Azerbaijan.
+        Bahamas, // Represents the country Bahamas.
+        Bahrain, // Represents the country Bahrain.
+        Bangladesh, // Represents the country Bangladesh.
+        Barbados, // Represents the country Barbados.
+        Belarus, // Represents the country Belarus.
+        Belgium, // Represents the country Belgium.
+        Belize, // Represents the country Belize.
+        Benin, // Represents the country Benin.
+        Bhutan, // Represents the country Bhutan.
+        Bolivia, // Represents the country Bolivia.
+        BosniaAndHerzegovina, // Represents the country Bosnia and Herzegovina.
+        Botswana, // Represents the country Botswana.
+        Brazil, // Represents the country Brazil.
+        BruneiDarussalam, // Represents the country Brunei Darussalam.
+        Bulgaria, // Represents the country Bulgaria.
+        BurkinaFaso, // Represents the country Burkina Faso.
+        Burundi, // Represents the country Burundi.
+        CaboVerde, // Represents the country Cabo Verde.
+        Cambodia, // Represents the country Cambodia.
+        Cameroon, // Represents the country Cameroon.
+        Canada, // Represents the country Canada.
+        CentralAfricanRepublic, // Represents the country Central African Republic.
+        Chad, // Represents the country Chad.
+        Chile, // Represents the country Chile.
+        China, // Represents the country China.
+        Colombia, // Represents the country Colombia.
+        Comoros, // Represents the country Comoros.
+        Congo, // Represents the country Congo.
+        CostaRica, // Represents the country Costa Rica.
+        CoteDIvoire, // Represents the country Cote d'Ivoire.
+        Croatia, // Represents the country Croatia.
+        Cuba, // Represents the country Cuba.
+        Cyprus, // Represents the country Cyprus.
+        CzechRepublic, // Represents the country Czech Republic.
+        DemocraticPeoplesRepublicOfKorea, // Represents the country Democratic People's Republic of Korea.
+        DemocraticRepublicOfTheCongo, // Represents the country Democratic Republic of the Congo.
+        Denmark, // Represents the country Denmark.
+        Djibouti, // Represents the country Djibouti.
+        Dominica, // Represents the country Dominica.
+        DominicanRepublic, // Represents the country Dominican Republic.
+        Ecuador, // Represents the country Ecuador.
+        Egypt, // Represents the country Egypt.
+        ElSalvador, // Represents the country El Salvador.
+        EquatorialGuinea, // Represents the country Equatorial Guinea.
+        Eritrea, // Represents the country Eritrea.
+        Estonia, // Represents the country Estonia.
+        Eswatini, // Represents the country Eswatini.
+        Ethiopia, // Represents the country Ethiopia.
+        Fiji, // Represents the country Fiji.
+        Finland, // Represents the country Finland.
+        France, // Represents the country France.
+        Gabon, // Represents the country Gabon.
+        Gambia, // Represents the country Gambia.
+        Georgia, // Represents the country Georgia.
+        Germany, // Represents the country Germany.
+        Ghana, // Represents the country Ghana.
+        Greece, // Represents the country Greece.
+        Grenada, // Represents the country Grenada.
+        Guatemala, // Represents the country Guatemala.
+        Guinea, // Represents the country Guinea.
+        GuineaBissau, // Represents the country Guinea-Bissau.
+        Guyana, // Represents the country Guyana.
+        Haiti, // Represents the country Haiti.
+        HolySee, // Represents the country Holy See.
+        Honduras, // Represents the country Honduras.
+        Hungary, // Represents the country Hungary.
+        Iceland, // Represents the country Iceland.
+        India, // Represents the country India.
+        Indonesia, // Represents the country Indonesia.
+        Iran, // Represents the country Iran.
+        Iraq, // Represents the country Iraq.
+        Ireland, // Represents the country Ireland.
+        Israel, // Represents the country Israel.
+        Italy, // Represents the country Italy.
+        Jamaica, // Represents the country Jamaica.
+        Japan, // Represents the country Japan.
+        Jordan, // Represents the country Jordan.
+        Kazakhstan, // Represents the country Kazakhstan.
+        Kenya, // Represents the country Kenya.
+        Kiribati, // Represents the country Kiribati.
+        Kuwait, // Represents the country Kuwait.
+        Kyrgyzstan, // Represents the country Kyrgyzstan.
+        LaoPeoplesDemocraticRepublic, // Represents the country Lao People's Democratic Republic.
+        Latvia, // Represents the country Latvia.
+        Lebanon, // Represents the country Lebanon.
+        Lesotho, // Represents the country Lesotho.
+        Liberia, // Represents the country Liberia.
+        Libya, // Represents the country Libya.
+        Liechtenstein, // Represents the country Liechtenstein.
+        Lithuania, // Represents the country Lithuania.
+        Luxembourg, // Represents the country Luxembourg.
+        Madagascar, // Represents the country Madagascar.
+        Malawi, // Represents the country Malawi.
+        Malaysia, // Represents the country Malaysia.
+        Maldives, // Represents the country Maldives.
+        Mali, // Represents the country Mali.
+        Malta, // Represents the country Malta.
+        MarshallIslands, // Represents the country Marshall Islands.
+        Mauritania, // Represents the country Mauritania.
+        Mauritius, // Represents the country Mauritius.
+        Mexico, // Represents the country Mexico.
+        Micronesia, // Represents the country Micronesia.
+        Monaco, // Represents the country Monaco.
+        Mongolia, // Represents the country Mongolia.
+        Montenegro, // Represents the country Montenegro.
+        Morocco, // Represents the country Morocco.
+        Mozambique, // Represents the country Mozambique.
+        Myanmar, // Represents the country Myanmar.
+        Namibia, // Represents the country Namibia.
+        Nauru, // Represents the country Nauru.
+        Nepal, // Represents the country Nepal.
+        Netherlands, // Represents the country Netherlands.
+        NewZealand, // Represents the country New Zealand.
+        Nicaragua, // Represents the country Nicaragua.
+        Niger, // Represents the country Niger.
+        Nigeria, // Represents the country Nigeria.
+        NorthMacedonia, // Represents the country North Macedonia.
+        Norway, // Represents the country Norway.
+        Oman, // Represents the country Oman.
+        Pakistan, // Represents the country Pakistan.
+        Palau, // Represents the country Palau.
+        Panama, // Represents the country Panama.
+        PapuaNewGuinea, // Represents the country Papua New Guinea.
+        Paraguay, // Represents the country Paraguay.
+        Peru, // Represents the country Peru.
+        Philippines, // Represents the country Philippines.
+        Poland, // Represents the country Poland.
+        Portugal, // Represents the country Portugal.
+        Qatar, // Represents the country Qatar.
+        RepublicOfKorea, // Represents the country Republic of Korea.
+        RepublicOfMoldova, // Represents the country Republic of Moldova.
+        Romania, // Represents the country Romania.
+        RussianFederation, // Represents the country Russian Federation.
+        Rwanda, // Represents the country Rwanda.
+        SaintKittsAndNevis, // Represents the country Saint Kitts and Nevis.
+        SaintLucia, // Represents the country Saint Lucia.
+        SaintVincentAndTheGrenadines, // Represents the country Saint Vincent and the Grenadines.
+        Samoa, // Represents the country Samoa.
+        SanMarino, // Represents the country San Marino.
+        SaoTomeAndPrincipe, // Represents the country Sao Tome and Principe.
+        SaudiArabia, // Represents the country Saudi Arabia.
+        Senegal, // Represents the country Senegal.
+        Serbia, // Represents the country Serbia.
+        Seychelles, // Represents the country Seychelles.
+        SierraLeone, // Represents the country Sierra Leone.
+        Singapore, // Represents the country Singapore.
+        Slovakia, // Represents the country Slovakia.
+        Slovenia, // Represents the country Slovenia.
+        SolomonIslands, // Represents the country Solomon Islands.
+        Somalia, // Represents the country Somalia.
+        SouthAfrica, // Represents the country South Africa.
+        SouthSudan, // Represents the country South Sudan.
+        Spain, // Represents the country Spain.
+        SriLanka, // Represents the country Sri Lanka.
+        StateOfPalestine, // Represents the country State of Palestine.
+        Sudan, // Represents the country Sudan.
+        Suriname, // Represents the country Suriname.
+        Sweden, // Represents the country Sweden.
+        Switzerland, // Represents the country Switzerland.
+        SyrianArabRepublic, // Represents the country Syrian Arab Republic.
+        Tajikistan, // Represents the country Tajikistan.
+        Thailand, // Represents the country Thailand.
+        TimorLeste, // Represents the country Timor-Leste.
+        Togo, // Represents the country Togo.
+        Tonga, // Represents the country Tonga.
+        TrinidadAndTobago, // Represents the country Trinidad and Tobago.
+        Tunisia, // Represents the country Tunisia.
+        Turkey, // Represents the country Turkey.
+        Turkmenistan, // Represents the country Turkmenistan.
+        Tuvalu, // Represents the country Tuvalu.
+        Uganda, // Represents the country Uganda.
+        Ukraine, // Represents the country Ukraine.
+        UnitedArabEmirates, // Represents the country United Arab Emirates.
+        UnitedKingdomOfGreatBritainAndNorthernIreland, // Represents the country United Kingdom of Great Britain and Northern Ireland.
+        UnitedRepublicOfTanzania, // Represents the country United Republic of Tanzania.
+        UnitedStatesOfAmerica, // Represents the country United States of America.
+        Uruguay, // Represents the country Uruguay.
+        Uzbekistan, // Represents the country Uzbekistan.
+        Vanuatu, // Represents the country Vanuatu.
+        Venezuela, // Represents the country Venezuela.
+        Vietnam, // Represents the country Vietnam.
+        Yemen, // Represents the country Yemen.
+        Zambia, // Represents the country Zambia.
+        Zimbabwe // Represents the country Zimbabwe.
     }
 }
