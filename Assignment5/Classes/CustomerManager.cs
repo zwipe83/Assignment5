133675d8822d16d358c4783896d4514e9ea34b61
Samuel Jeffman <sjeffman83@outlook.com>
2024-04-07 21:52:41 +0200

    Implemented basic CustomerManager class

--- Assignment5/Classes/CustomerManager.cs
+++ Assignment5/Classes/CustomerManager.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Assignment5.Helpers.Debugger;
 
 namespace Assignment5.Classes
 {
-    internal class CustomerManager
+    public class CustomerManager
     {
+        private List<Customer> _customerList = new List<Customer>();
+
+        public List<Customer> CustomerList
+        {
+            get => _customerList;
+            set => _customerList = value; //TODO: Add check Index method
+        }
+
+        public void AddNew(Customer customer)
+        {
+            CustomerList.Add(customer);
+
+            DEBUG_PRINT($"Customer list count: {CustomerList.Count}");
+        }
     }
 }
