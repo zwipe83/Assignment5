1707d47eac3d5ce43a5e13f86ec9549a9b200180
Samuel Jeffman <sjeffman83@outlook.com>
2024-04-07 12:20:13 +0200

    Reworked certain areas

--- Assignment5/Classes/Contact.cs
+++ Assignment5/Classes/Contact.cs
@@ -5,140 +5,115 @@
 /// </summary>
 /// 
 
-using static Assignment5.Helpers.EmailChecker;
 using static Assignment5.Helpers.Debugger;
-using System.Security.Policy;
 
 namespace Assignment5.Classes
 {
-    public class EmailAddress
-    {
-        private string? _address;
-        public string? Address 
-        { 
-            get => _address; 
-            set
-            {
-                if(IsValidAddress(value))
-                {
-                    _address = value;
-                    DEBUG_PRINT($"EmailAddress.Address - Correct address: {value}");
-                }
-                else
-                {
-                    //TODO: Throw exception?
-                    DEBUG_PRINT($"EmailAddress.Address - Incorrect address: {value}");
-                }
-            }
-        }
-
-        public EmailAddress() 
-        { 
-        
-        }
-
-        public EmailAddress(string email) 
-        { 
-            Address = email;
-        }
-    }
-    public class PhoneNumber
-    {
-        public PhoneNumber() { }
-    }
-    public class Address
-    {
-        public Address() { }
-    }
-
-    public class Name
-    {
-        private string? _name;
-        private bool _isLastName = false;
-
-        public bool IsLastName
-        {
-            get => _isLastName;
-            set => _isLastName = value;
-        }
-
-        public string? NameProp
-        {
-            get => _name;
-            set => _name = value; 
-        }
-        public Name() 
-        { 
-            
-        }
-    }
 
     public class Contact
     {
-        private EmailAddress? _businessEmail = new EmailAddress();
-        private EmailAddress? _privateEmail = new();
-        private PhoneNumber? _homePhone = new();
-        private PhoneNumber? _mobilePhone = new();
-        private Address? _address = new();
-        private Name? _firstName = new();
-        private Name? _lastName = new();
+        #region Fields
+        private Email? email;
+        private Phone? phone;
+        private Address? address;
 
-        public EmailAddress? BusinessEmail
-        {
-            get => _businessEmail;
-            set => _businessEmail = value;
-        }
-        public EmailAddress? PrivateEmail
-        {
-            get => _privateEmail;
-            set => _privateEmail = value;
-        }
-        public PhoneNumber? HomePhone
-        {
-            get => _homePhone;
-            set => _homePhone = value;
-        }
-        public PhoneNumber? MobilePhone
-        {
-            get => _mobilePhone;
-            set => _mobilePhone = value;
-        }
-        public Address? Address
-        {
-            get => _address;
-            set => _address = value;
-        }
-        public Name? FirstName
+        private string _firstName;
+        private string _lastName;
+        #endregion
+        #region Properties
+        public string FirstName
         {
             get => _firstName;
             set => _firstName = value;
         }
-        public Name? LastName
+        public string LastName
         {
             get => _lastName;
-            set
-            {
-                _lastName = value;
-                if (_lastName is not null)
-                {
-                    _lastName.IsLastName = true;
-                }
-            }
+            set => _lastName = value;
         }
 
-        public Contact()
+        public Address AddressData
         {
-        }
-        public Contact(string firstName) : this(firstName, string.Empty)
-        {
-            FirstName.NameProp = firstName;
-        }
-        public Contact(string firstName, string lastName) : this(firstName, lastName, string.Empty)
-        {
-        }
-        public Contact(string firstName, string lastName, string businessEmail) : this(firstName, lastName, businessEmail, )
-        {
+            get => address;
+            set => address = value;
         }
 
+        public Email EmailData
+        {
+            get => email;
+            set => email = value;
+        }
+
+        public Phone PhoneData
+        {
+            get => phone;
+            set => phone = value;
+        }
+        #endregion
+        #region Constructors
+        /// <summary>
+        /// Base constructor, creates an intance of <see cref="Contact"/> with default values
+        /// </summary>
+        public Contact() : this(string.Empty, string.Empty, new Address(), new Phone(), new Email())
+        {
+        }
+        /// <summary>
+        /// Inits a new object of type <see cref="Contact">
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="address"></param>
+        /// <param name="phone"></param>
+        /// <param name="email"></param>
+        public Contact(string firstName, string lastName, Address address, Phone phone, Email email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            AddressData = address;
+            PhoneData = phone;
+            EmailData = email;
+        }
+        /// <summary>
+        /// Copy Constructor, copies values from one <see cref="Contact"/> to a new instance of <see cref="Contact"/>
+        /// </summary>
+        /// <param name="objToCopyFrom"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public Contact(Contact objToCopyFrom)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        #region Methods
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        private bool CheckData()
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        private string GetAddressInfo()
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            string contactData = $"First name: {FirstName}, last name: {LastName} - Address: {AddressData} - Email: {EmailData}";
+
+            DEBUG_PRINT($"Contact.ToString(): {contactData}");
+
+            return contactData;
+        }
+        #endregion
     }
 }
