6e08577aecf3cb112d3644ce1f649d93c1045760
Samuel Jeffman <sjeffman83@outlook.com>
2024-04-11 20:46:58 +0200

    Added an interface to play around with it a bit, get a feel for how it works.

---
+++ Assignment5/Interfaces/IManager.cs
@@ -0,0 +1,24 @@
+﻿using Assignment5.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5.Interfaces
+{
+    internal interface ICustomerManager
+    {
+        public void AddCustomer(Contact contact);
+        public void ChangeCustomer(Contact contact, Guid guid);
+        public bool CheckIndex(Guid guid);
+        public void Remove(Guid guid);
+        public void DeleteCustomer(Guid guid);
+        public void GetAddressInfo(Guid guid);
+        public Customer GetCustomer(string customerInfo);
+        public string[] GetCustomerInfoStrings();
+        public string GetEmailInfo();
+        public string GetPhoneInfo();
+        public void TestValues();
+    }
+}
